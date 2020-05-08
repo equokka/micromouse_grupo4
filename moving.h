b0e0201d13d910e3e84ef34d4ee745f70f49a427
vozk1 <vozki123@gmail.com>
2020-05-08 09:30:52 -0400

    Add dos ficheiros c)

---
+++ moving.h
@@ -0,0 +1,12 @@
+void clearscreen();
+
+void cbreak();
+
+void nocbreak();
+
+int *up(int **maze, int *pos);
+int *down(int **maze, int *pos);
+int *right(int **maze, int *pos);
+int *left(int **maze, int *pos);
+
+int mapa(int **maze);
